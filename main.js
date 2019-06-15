c995d1a6540a2f9ab85de17408f2ad0959d45287
Latinitas <latinitas@latinitassmbp3.attlocal.net>
2019-06-15 14:06:39 -0500

    added buttons through js

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("btn1").addEventListener("click",myName)
+function myName() {
+    document.getElementById("name").innerHTML="Semra"
+}
+document.getElementById("btn2").addEventListener("click",school)
+function school() {
+    document.getElementById("school").innerHTML="Harmony School of Endeavor"
+}
+document.getElementById("btn3").addEventListener("click",career)
+function career() {
+    document.getElementById("career").innerHTML=""
+}
+document.getElementById("btn4").addEventListener("click",hobbies)
+function hobbies() {
+    document.getElementById("hobbies").innerHTML="Listening music, skating, drawing"
+}
